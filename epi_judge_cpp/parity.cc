8f831048b93555a633e9669f055a7c403786988b
suneeshs <suneesh.sk@gmail.com>
2020-01-21 21:55:15 -0500

    4.1

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,9 +1,21 @@
 #include "test_framework/generic_test.h"
 short Parity(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
-}
+    short result = 0;
 
+    /*while (x) {
+        result ^= (x & 1);
+        x >>= 1;
+    }*/
+
+    while (x) {
+        result ^= 1;
+        x &= (x - 1);
+    }
+
+
+    return result;
+}
+ 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x"};
